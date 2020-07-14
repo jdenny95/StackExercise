c6bd3bed8cca44901e024060bfef6f72dd733cd7
jdenny95 <51931801+jdenny95@users.noreply.github.com>
2020-07-14 20:55:51 +0100

    Add files via upload

---
+++ StackExercise/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+            var min = 0;
+            var max = 10;
+
+            for (var i = min; i <= max; i++)
+                stack.Push(i);
+
+            for (var i = min; i <= max; i++)
+                Console.WriteLine(stack.Pop());
+
+            Console.WriteLine("Please type 'clear' to clear the list or 'exit' to close application.");
+
+            while (true)
+            {
+                try
+                {
+                    var input = Console.ReadLine();
+
+                    switch (input.ToLower())
+                    {
+                        case "clear":
+                            stack.Clear();
+                            Console.WriteLine("The list has been cleared");
+                            Console.WriteLine(stack.Pop()); // To prove that the list has been cleared
+                            break;
+                        case "exit":
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("I dont know that command. Please type 'clear' to clear the list or 'exit' to close application.");
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ StackExercise/Stack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace StackExercise
+{
+    public class Stack
+    {
+        private readonly List<object> _list = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("The object sent to stack can't be null");
+
+            _list.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (_list.Count == 0)
+                throw new InvalidOperationException("The list must have objects first");
+
+            var lastIndex = _list.Count - 1;
+
+            var returnObjects = _list[lastIndex];
+
+            _list.RemoveAt(lastIndex);
+
+            return returnObjects;
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+
+        public void Exit()
+        {
+            Environment.Exit(0);
+        }
+    }
+}
